b6015bf12acccce747a165b18cdc4e246bb4df1c
Abdul Halim Arif <53304120+ahadaulay@users.noreply.github.com>
2024-08-22 03:17:38 +0700

    add model,seed, migration, relation, auth

--- database/migrations/2024_08_21_175801_create_tools_table.php
+++ database/migrations/2024_08_21_175801_create_tools_table.php
@@ -11,16 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tools', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('avatar');
-            $table->string('occupation');
-            $table->unsignedBigInteger('connect');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('slug');
+            $table->string('icon');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tools');
     }
 };
